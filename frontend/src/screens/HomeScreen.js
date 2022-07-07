2134d50c0aaef76c1ffd111e3b974aab92574c3f
Your Name <you@example.com>
2022-07-07 16:26:53 +0300

    add react-bootstrap

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,7 @@ import React, { useEffect, useReducer } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import logger from 'use-reducer-logger';
+import Container from 'react-bootstrap/esm/Container';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -39,27 +40,25 @@ function HomeScreen() {
   }, []);
   return (
     <div>
-      <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {products.map((product) => (
-            <div className="product" key={product.slug}>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {products.map((product) => (
+          <div className="product" key={product.slug}>
+            <Link to={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+            </Link>
+            <div className="product-info">
               <Link to={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
+                <p>{product.name}</p>
               </Link>
-              <div className="product-info">
-                <Link to={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </Link>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
+              <p>
+                <strong>${product.price}</strong>
+              </p>
+              <button>Add to cart</button>
             </div>
-          ))}
-        </div>
-      </main>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
