7f8a834b144b47c9c2ad37f3a4b32ff62fc90607
Your Name <you@example.com>
2022-07-07 15:02:49 +0300

    Add state managment with Reducer

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,22 +1,48 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import logger from 'use-reducer-logger';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FRETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 function HomeScreen() {
-  const [product, setProduct] = useState([]);
+  //const [products, setProducts] = useState([]);
+  const [{ loading, products, error }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    error: '',
+    loading: true,
+  });
+
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get('/api/products');
-      setProduct(result.data);
+      dispatch({ type: 'FRETCH_REQUEST' });
+      try {
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
+      //setProducts(result.data);
     };
     fetchData();
-  });
+  }, []);
   return (
     <div>
       <main>
         <h1>Featured Products</h1>
         <div className="products">
-          {product.map((product) => (
+          {products.map((product) => (
             <div className="product" key={product.slug}>
               <Link to={`/product/${product.slug}`}>
                 <img src={product.image} alt={product.name} />
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -7,7 +7,7 @@ function ProductScreen() {
 
   return (
     <div>
-      <h1>{slug }</h1>
+      <h1>{slug}</h1>
     </div>
   );
 }
